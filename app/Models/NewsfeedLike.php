f3f3b608794c96b8b5f955c375c13d22fab54d58
lieking029 <johnmichaelbuan21@gmail.com>
2024-10-22 10:40:49 +0800

    newsfeed likes

--- app/Models/NewsfeedLike.php
+++ app/Models/NewsfeedLike.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class NewsfeedLike extends Model
 {
     /** @use HasFactory<\Database\Factories\NewsfeedLikeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'newsfeed_id',
+        'status',
+    ];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function newsfeed() : BelongsTo
+    {
+        return $this->belongsTo(Newsfeed::class);
+    }
 }
